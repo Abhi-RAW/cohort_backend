e659fffdec28d5d4757df1e01a67fd234cacb063
Abhiraj R S <149815416+Abhi-RAW@users.noreply.github.com>
2025-01-29 19:25:27 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -8,16 +8,6 @@ const { wishlistRouter } = require("./wishlistRoutes.js");
 const { cartRouter } = require("./cartRoutes.js");
 const { orderRouter } = require("./orderRoutes.js");
 const { reviewRouter } = require("./reviewRoutes.js");
-// const { productRouter} = require("./productRoutes.js");
-// const { cartRouter} = require("./cartRoutes.js");
-// const { wishlistRouter} = require("./wishlistRoutes.js");
-// const { orderRouter } = require("./orderRoutes.js");
-// const { paymentRouter } = require("./paymentRoutes.js");
-// const { adminRouter } = require("./adminRoutes.js");
-// const { sellerRouter } = require("./sellerRoutes.js");
-// const { reviewRouter } = require("./reviewRoutes.js");
-// const { searchRouter } = require("./searchRoutes.js");
-
 const router = express.Router();
 
 router.use('/users', userRouter);
@@ -28,15 +18,7 @@ router.use('/wishlist', wishlistRouter);
 router.use('/cart', cartRouter);
 router.use('/order', orderRouter);
 router.use('/review', reviewRouter);
-// router.use("/products", productRouter);
-// router.use("/cart", cartRouter); 
-// router.use("/wishlist", wishlistRouter);
-// router.use("/orders", orderRouter);
-// router.use("/payments", paymentRouter);
-// router.use("/admin", adminRouter);
-// router.use("/seller", sellerRouter);
-// router.use("/reviews", reviewRouter); 
-// router.use("/search", searchRouter);  
+
 
 // module.exports = router ;
 module.exports = { apiRouter: router};
